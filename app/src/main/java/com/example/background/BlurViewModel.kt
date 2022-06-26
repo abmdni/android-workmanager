26c0bb60da6d01011636579be73f1cfdd9caca4a
abmdni <abdessamad.yadini@gmail.com>
2022-06-26 22:58:58 +0100

    to ensure one chain of work run at a time

--- app/src/main/java/com/example/background/BlurViewModel.kt
+++ app/src/main/java/com/example/background/BlurViewModel.kt
@@ -22,10 +22,7 @@ import android.content.Context
 import android.net.Uri
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import androidx.work.Data
-import androidx.work.OneTimeWorkRequest
-import androidx.work.OneTimeWorkRequestBuilder
-import androidx.work.WorkManager
+import androidx.work.*
 import com.example.background.workers.BlurWorker
 import com.example.background.workers.CleanupWorker
 import com.example.background.workers.SaveImageToFileWorker
@@ -46,8 +43,16 @@ class BlurViewModel(application: Application) : ViewModel() {
      * @param blurLevel The amount to blur the image
      */
     internal fun applyBlur(blurLevel: Int) {
+       // var continuation = workManager
+       //     .beginWith(OneTimeWorkRequest.from(CleanupWorker::class.java))
+
+        // to ensure one work at a time ->
         var continuation = workManager
-            .beginWith(OneTimeWorkRequest.from(CleanupWorker::class.java))
+            .beginUniqueWork(
+                IMAGE_MANIPULATION_WORK_NAME,
+                ExistingWorkPolicy.REPLACE,
+                OneTimeWorkRequest.from(CleanupWorker::class.java)
+            )
 
         //workManager.enqueue(OneTimeWorkRequest.from(BlurWorker::class.java))
        // val blurRequest = OneTimeWorkRequestBuilder<BlurWorker>()
